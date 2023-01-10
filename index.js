8cd58e9456ef44b343610b7e7d81e9e1f9cb7b72
Jurasia <jurasiabritton@gmail.com>
2023-01-09 21:29:17 -0500

    done

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+
 const divide = () => {return 2000/100}
 
+  
+  
 
-const square = (x) =>  {return x*x}; 
+const square = (x) =>  {return x*x};
+
 let x = 2
+
+  
 
 const add = (a,b) => {return a+b}
 
+  
+
 let a = 2000
-    let b = 100
+
+ let b = 100
  
 
     
